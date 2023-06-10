d86abae69d13fea7e20a945a04d48f312c95ccf7
gev_org <thecarguyarm@gmail.com>
2023-06-10 22:51:16 +0400

    removed comments

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,11 +1,3 @@
-// export interface Person {
-//     firstName: string;
-//     lastName: string;
-// }
-
-// export interface ExtendedPerson extends Person {
-//     age: number
-// }
 
 export interface Person {
     name: string;
